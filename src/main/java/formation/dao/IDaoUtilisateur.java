380f7caf579ebc9908435e8c4afd6990dd51c4b2
Elodie <elodie.linhart@free.fr>
2019-01-10 17:14:28 +0100

    IDaoUtilisateurs

--- src/main/java/formation/dao/IDaoUtilisateur.java
+++ src/main/java/formation/dao/IDaoUtilisateur.java
@@ -2,9 +2,11 @@ package formation.dao;
 
 import java.util.List;
 
+import formation.model.Patient;
 import formation.model.Praticien;
 import formation.model.Utilisateur;
 
 public interface IDaoUtilisateur extends IDao<Utilisateur,Long> {
-	public List<Praticien> findAllPraticien();
+	List<Praticien> findAllPraticien();
+	List<Patient> findAllPatient();
 }
