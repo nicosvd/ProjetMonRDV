df54240e2f936bf2845387daa0c2a8b78301e2b4
nicosvd <nico.svd@orange.fr>
2019-01-10 16:18:35 +0100

    ignore

--- src/main/java/formation/model/Patient.java
+++ src/main/java/formation/model/Patient.java
@@ -9,107 +9,89 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-
 
 @Entity
 @DiscriminatorValue("patient")
 
-public class Patient extends Utilisateur{
-	@Column (name="phone")
+public class Patient {
+	@Column(name = "phone")
 	private Integer telephone;
-	@Column (name="birthDate")
+	@Column(name = "birthDate")
 	private Date dtNaissance;
 	private boolean principal;
-	@Column (name="lastname")
+	@Column(name = "lastname")
 	private String nom;
-	@Column (name="firstname")
+	@Column(name = "firstname")
 	private String prenom;
 	@OneToOne
 	@JoinColumn
 	private Adresse adresse;
-//	@ManyToOne
-//	private Utilisateur utilisateur;
+	@ManyToOne
+	private Utilisateur utilisateur;
 
-	
-	
 	public Patient() {
 		super();
 	}
 
-
-	public Patient(String mail, String motdepasse, TypeUtilisateur typeUtilisateur, Integer telephone, Date dtNaissance, boolean principal, String nom, String prenom) {
-		super(mail, motdepasse, typeUtilisateur);
+	public Patient(String mail, String motdepasse, TypeUtilisateur typeUtilisateur, Integer telephone, Date dtNaissance,
+			boolean principal, String nom, String prenom) {
+		super();
 		this.telephone = telephone;
 		this.dtNaissance = dtNaissance;
 		this.principal = principal;
 		this.nom = nom;
 		this.prenom = prenom;
 	}
-
 
 	public Integer getTelephone() {
 		return telephone;
 	}
 
-
 	public void setTelephone(Integer telephone) {
 		this.telephone = telephone;
 	}
-
 
 	public Date getDtNaissance() {
 		return dtNaissance;
 	}
 
-
 	public void setDtNaissance(Date dtNaissance) {
 		this.dtNaissance = dtNaissance;
 	}
-
 
 	public boolean isPrincipal() {
 		return principal;
 	}
 
-
 	public void setPrincipal(boolean principal) {
 		this.principal = principal;
 	}
-
 
 	public String getNom() {
 		return nom;
 	}
 
-
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
 
 	public String getPrenom() {
 		return prenom;
 	}
 
-
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
-
 
 	public Adresse getAdresse() {
 		return adresse;
 	}
 
-
 	public void setAdresse(Adresse adresse) {
 		this.adresse = adresse;
 	}
 
-	
-	
-	
-	
 }
